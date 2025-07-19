686d147ca521e17a5ef723e717cd9fbfe4b878d5
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2025-07-19 19:53:39 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router"
+
+function App() {
+
+  return (
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
+      </Routes>
+  )
+}
+
+export default App
